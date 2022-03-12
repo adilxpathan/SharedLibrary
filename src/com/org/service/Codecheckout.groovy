33cfcdbfec112fe100ced9fec21c2a498556ff92
pathanadil <44292489+pathanadil@users.noreply.github.com>
2022-03-12 23:36:54 +0530

    Update Codecheckout.groovy

--- src/com/org/service/Codecheckout.groovy
+++ src/com/org/service/Codecheckout.groovy
@@ -1,13 +1,13 @@
 package com.org.service
-class Codecheckout implements Serializable{
+public class Codecheckout implements Serializable{
 Script mainScript
 Map specs
 
-  def Codecheckout(Script mainScript, Map specs){
+  string Codecheckout(Script mainScript, Map specs){
   this.mainScript = mainScript
   this.specs = specs
   }
-  def checkOutFunc(Map specs){
+  string checkOutFunc(Map specs){
     mainScript.checkout([$class: 'GitSCM',
     branches: [[name: specs.branch]],
     extensions: [],
