b931d0e1decaa3b331e86cd4ce6c89f86fe10690
pathanadil <44292489+pathanadil@users.noreply.github.com>
2022-03-13 11:49:36 +0530

    Update Codecoverage.groovy

--- src/com/org/service/Codecoverage.groovy
+++ src/com/org/service/Codecoverage.groovy
@@ -1,16 +1,32 @@
 package com.org.service
+
+//import groovy.transform.Field
+//import com.toyota.tfs.exception.*
+import com.org.log.Logger
+import com.org.log.LogLevel
+
 class Codecoverage implements Serializable{
 Script mainScript
 Map specs
 Map config
+Logger logger
 
   def Codecoverage(Script mainScript, Map specs, Map config){
   this.mainScript = mainScript
   this.specs = specs
   this.config = config
+  this.logger = new Logger(mainScript,"Codecoverage")
   }
   def codecoverageCheckFunc(Map specs, Map config){
-    mainScript.sh config.java.codecoverage.jacoco.command 
-    mainScript.publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: './target/site/jacoco/', reportFiles: 'index.html', reportName: 'Code Coverage Report', reportTitles: ''])
-  }
+    if (specs.containsKey("codeCoverage")) {
+      if (specs.codeCoverage.tool == "jacoco") {
+        mainScript.sh config.java.codecoverage.jacoco.command 
+        mainScript.publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: './target/site/jacoco/', reportFiles: 'index.html', reportName: 'Code Coverage Report', reportTitles: ''])
+        } else {
+          logger.warn "unsupported tool. Please use jacoco."
+        }
+      } else {
+          logger.warn "Skipping Codecoverage stage as specs are missing."
+        }
+    }
 }
