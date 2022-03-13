a68acbe5ee00f730eb37a02ccd23fac7baf1855f
pathanadil <44292489+pathanadil@users.noreply.github.com>
2022-03-13 12:12:05 +0530

    Update Codecoverage.groovy

--- src/com/org/service/Codecoverage.groovy
+++ src/com/org/service/Codecoverage.groovy
@@ -18,15 +18,16 @@ Logger logger
   this.logger = new Logger(mainScript,"Codecoverage")
   }
   def codecoverageCheckFunc(Map specs, Map config){
-    if (specs.containsKey("codeCoverage")) {
+    if (specs.codeCoverage.isCodecoverageRequired && specs.containsKey("codeCoverage")){  
       if (specs.codeCoverage.tool == "jacoco") {
         mainScript.sh config.java.codecoverage.jacoco.command 
         mainScript.publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: './target/site/jacoco/', reportFiles: 'index.html', reportName: 'Code Coverage Report', reportTitles: ''])
-        } else {
+        logger.info "codeCoverage successfully completed."
+      } else {
           logger.warn "unsupported tool. Please use jacoco."
         }
       } else {
-          logger.warn "Skipping Codecoverage stage as specs are missing."
+          logger.warn "Skipping code coverage stage because code coverage templates are missing or code coverage stage is disabled."
         }
     }
 }
