ceb80960ab2a62bde9670a4fa2a11cd4ac9eb021
pathanadil <44292489+pathanadil@users.noreply.github.com>
2022-03-13 12:32:28 +0530

    Update java_jenkinsfile.groovy

--- vars/java_jenkinsfile.groovy
+++ vars/java_jenkinsfile.groovy
@@ -1,5 +1,11 @@
+//import groovy.transform.Field
+//import com.toyota.tfs.exception.*
+import com.org.log.Logger
+import com.org.log.LogLevel
+
 def call(){
   node('master') {
+    Logger logger = new Logger(this, "Java-Jenkinsfile", LogLevel.fromString(env.LOG_LEVEL))
     def specs = [:]
     try {
     stage('Specs Checkout'){
@@ -8,66 +14,53 @@ def call(){
       repo: Repo,
       branch: Branch
       ])
-      stage('reading GlobalConfig & Specs'){ 
-        try {
-          println "reading the specs from Specs repository"
-          def specsDir = "./$Version"
-          println "specs version" + specsDir
-            if(fileExists(specsDir + "/ci_template.yaml")){
-             ci_template = readYaml file : specsDir + "/ci_template.yaml"
-             specs = specs + ci_template
-             println "reading specs file" + specs
-              }
-         
-            println "reading the global config from resources"
-            def request = libraryResource "com/org/service/globalConfig/globalConfig.yaml"
-            config = readYaml text: request
-            println "reading config file" + config
-            }
-        catch(Exception e) {
-             println "Error in reading specs file : " + e.getMessage()
-        throw e
-            }
-      }
+        stage('reading GlobalConfig & Specs'){ 
+            try {
+            logger.info "reading the specs from Specs repository"
+            def specsDir = "./$Version"
+            logger.debug "specs version" + specsDir
+                if(fileExists(specsDir + "/ci_template.yaml")){
+                ci_template = readYaml file : specsDir + "/ci_template.yaml"
+                specs = specs + ci_template
+                logger.debug "reading specs file" + specs
+                }
+            
+                logger.info "reading the global config from resources"
+                def request = libraryResource "com/org/service/globalConfig/globalConfig.yaml"
+                config = readYaml text: request
+                logger.debug "reading config file" + config
+                }
+            catch(Exception e) {
+                logger.error "Error in reading specs file : " + e.getMessage()
+            throw e
+                }
+        }
      
-    stage('Code Checkout'){
-        ciFunc.checkoutVarFunc([
-        repo: specs.scm.repo,
-        branch: specs.scm.branch  
-          ])
+        stage('Code Checkout'){
+            ciFunc.checkoutVarFunc([
+            repo: specs.scm.repo,
+            branch: specs.scm.branch  
+            ])
+            }
+        }   
+        stage('Build'){
+        ciFunc.build(specs, config)
+        }
+        
+        stage('UnitTest'){
+            ciFunc.unittest(specs, config)
+        }
+
+        stage('CodeCoverage'){
+        ciFunc.codecoverage(specs, config)
+        }
+
+        stage('CodeQuality'){
+        ciFunc.codequality(specs, config)
         }
     }   
-    stage('Build'){
-      ciFunc.build(specs, config)
-      }
-      if (specs.unitTest.isUnittestRequired && specs.containsKey("unitTest")){
-      stage('UnitTest'){
-        ciFunc.unittest(specs, config)
-      }
-   }
-      else {
-      println "Skipping unit test stage because unit Test templates are missing or Unit Test stage is disabled."
-      }
-    if (specs.codeCoverage.isCodecoverageRequired && specs.containsKey("codeCoverage")){  
-    stage('CodeCoverage'){
-      ciFunc.codecoverage(specs, config)
-      }
-    } 
-    else {
-    println "Skipping code coverage stage because code coverage templates are missing or code coverage stage is disabled." 
-      }
-      
-    if (specs.codeQuality.isCodeQualityRequired && specs.containsKey("codeQuality")){  
-    stage('CodeQuality'){
-      ciFunc.codequality(specs, config)
-      }
-    } 
-    else {
-    println "Skipping code quality stage because code quality templates are missing or code quality stage is disabled." 
-      }  
-    }   
     catch(Exception e) {
-      println "Error in build stage : " + e.getMessage()
+      logger.error "Error in build stage : " + e.getMessage()
     throw e
       }
     }
