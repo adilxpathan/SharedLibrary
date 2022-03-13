5a8b4c35abb72a1ca2f36af1a380ddb3de40ac8e
pathanadil <44292489+pathanadil@users.noreply.github.com>
2022-03-13 12:05:02 +0530

    Update Unittesting.groovy

--- src/com/org/service/Unittesting.groovy
+++ src/com/org/service/Unittesting.groovy
@@ -1,24 +1,34 @@
 package com.org.service
+
+//import groovy.transform.Field
+//import com.toyota.tfs.exception.*
+import com.org.log.Logger
+import com.org.log.LogLevel
+
 class Unittesting implements Serializable{
 Script mainScript
 Map specs
 Map config
+Logger logger
 
   def Unittesting(Script mainScript, Map specs, Map config){
   this.mainScript = mainScript
   this.specs = specs
   this.config = config
+  this.logger = new Logger(mainScript,"Unittesting")
   }
   def unitTestFunc(Map specs, Map config) {
-    if (specs.containsKey("unitTest")) {
-        if (specs.unitTest.isUnittestRequired) {
+    if (specs.unitTest.isUnittestRequired && specs.containsKey("unitTest")){
+        if (specs.unitTest.tool == 'junit') {
             mainScript.sh config.java.unittest.junit.command 
             mainScript.sh config.java.unittest.junit.surefire
             mainScript.publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: './target/site/', reportFiles: 'surefire-report.html', reportName: 'UnitTest Report', reportTitles: '']) 
-          }   
+          } else {
+                logger.warn "unsupported tool. Please use junit."
+            } 
     } 
     else {
-    println "Skipping unit test stage as specs are missing."
+    logger.warn "Skipping unit test stage because unit Test templates are missing or Unit Test stage is disabled."
         } 
   }
 }
