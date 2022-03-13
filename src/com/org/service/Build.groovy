71106a686c79ccfbcf1fb7b50dd99522dceee37d
pathanadil <44292489+pathanadil@users.noreply.github.com>
2022-03-13 11:41:18 +0530

    Update Build.groovy

--- src/com/org/service/Build.groovy
+++ src/com/org/service/Build.groovy
@@ -1,13 +1,21 @@
 package com.org.service
+
+//import groovy.transform.Field
+//import com.toyota.tfs.exception.*
+import com.org.log.Logger
+import com.org.log.LogLevel
+
 class Build implements Serializable{
 Script mainScript
 Map specs
 Map config
+Logger logger
 
   def Build(Script mainScript, Map specs, Map config){
   this.mainScript = mainScript
   this.specs = specs
   this.config = config
+  this.logger = new Logger(mainScript,"Build")
   }
   def buildFunc(Map specs, Map config){
     if (specs.containsKey("build")) {
@@ -15,11 +23,11 @@ Map config
           mainScript.sh config.java.build.maven.command 
         }
         else {
-        println "unsupported tool. Please use Maven."
+        logger.warn "unsupported tool. Please use Maven."
             }  
     }
     else {
-    println "Skipping build stage as specs are missing."
+    logger.warn "Skipping build stage as specs are missing."
     throw new Exception("stopping the pipeline since build is skipped.")
         }
       }
