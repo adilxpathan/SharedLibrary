f07259fadea8bc6435d01909fe0606b4e83fdd6b
pathanadil <44292489+pathanadil@users.noreply.github.com>
2022-03-13 11:06:36 +0530

    Update Codecheckout.groovy

--- src/com/org/service/Codecheckout.groovy
+++ src/com/org/service/Codecheckout.groovy
@@ -1,17 +1,32 @@
 package com.org.service
+
+//import groovy.transform.Field
+//import com.toyota.tfs.exception.*
+import com.toyota.tfs.log.Logger
+import com.toyota.tfs.log.LogLevel
+
+
 public class Codecheckout implements Serializable{
 Script mainScript
 Map specs
+Logger logger
 
   def Codecheckout(Script mainScript, Map specs){
   this.mainScript = mainScript
   this.specs = specs
+  this.logger = new Logger(mainScript,"Codecheckout")
   }
   def checkOutFunc(Map specs){
-    mainScript.checkout([$class: 'GitSCM',
-    branches: [[name: specs.branch]],
-    extensions: [],
-    userRemoteConfigs: [[url: specs.repo ]]
-    ])
-  }
+    try {
+      mainScript.checkout([$class: 'GitSCM',
+      branches: [[name: specs.branch]],
+      extensions: [],
+      userRemoteConfigs: [[url: specs.repo ]]
+      ])
+      logger.info "Code succesfully checkedout from: " + specs.branch
+    }
+  } catch(Exception e){
+            throw new Exception("Error in Codecheckout: " + e.getMessage())
+        }
+  
 }
