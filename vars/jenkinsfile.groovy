152737a8e1e5c572742329613e8d52484bec80f1
pathanadil <44292489+pathanadil@users.noreply.github.com>
2022-03-14 11:59:05 +0530

    Create jenkinsfile.groovy

---
+++ vars/jenkinsfile.groovy
@@ -0,0 +1,53 @@
+//import groovy.transform.Field
+//import com.toyota.tfs.exception.*
+import com.org.log.Logger
+import com.org.log.LogLevel
+
+def call(){
+  node('master') {
+    Logger logger = new Logger(this, "Java-Jenkinsfile", LogLevel.fromString(env.LOG_LEVEL))
+    def specs = [:]
+    try {
+    stage('Specs Checkout'){
+      cleanWs()
+      ciFunc.checkoutVarFunc([
+      repo: Repo,
+      branch: Branch
+      ])
+        stage('reading GlobalConfig & Specs'){ 
+            try {
+            logger.info "reading the specs from Specs repository"
+            def specsDir = "./$Version"
+            logger.debug "specs version" + specsDir
+                if(fileExists(specsDir + "/ci_template.yaml")){
+                ci_template = readYaml file : specsDir + "/ci_template.yaml"
+                specs = specs + ci_template
+                logger.debug "reading specs file" + specs
+                }
+            
+                logger.info "reading the global config from resources"
+                def request = libraryResource "com/org/service/globalConfig/globalConfig.yaml"
+                config = readYaml text: request
+                logger.debug "reading config file" + config
+
+                if (specs.build.type == "java") {
+                  ciFunc.jenkinsfile(specs, config)
+                }
+                else {
+                logger.warn "unsupported technology, as of now we only supports Java."
+                    }  
+              
+                }
+            catch(Exception e) {
+                logger.error "Error in reading specs file : " + e.getMessage()
+            throw e
+                }
+        }
+     
+    }   
+    catch(Exception e) {
+      logger.error "Error in build stage : " + e.getMessage()
+    throw e
+      }
+    }
+  }
