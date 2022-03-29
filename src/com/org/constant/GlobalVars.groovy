adc038662852df8afed8984f9f73d51b7fdde1be
Adil Pathan <Adil.Pathan@lntinfotech.com>
2022-03-29 12:56:50 +0530

    GlobalVar changes

---
+++ src/com/org/constant/GlobalVars.groovy
@@ -0,0 +1,20 @@
+
+package com.org.constant
+
+class GlobalVars {
+
+    static Boolean publishHtml_allowMissing = false 
+    static Boolean publishHtml_alwaysLinkToLastBuild = false 
+    static Boolean publishHtml_keepAll = false 
+
+
+    static String publishHtml_unitTest_reportDir = './target/site/'
+    static String publishHtml_unitTest_reportFiles = 'surefire-report.html'
+    static String publishHtml_unitTest_reportName = 'UnitTest Report'
+
+    static String publishHtml_codeCoverage_reportDir = './target/site/jacoco/'
+    static String publishHtml_codeCoverage_reportFiles = 'index.html'
+    static String publishHtml_codeCoverage_reportName = 'Code Coverage Report'
+
+    static String publishHtml_reportTitles = '' 
+}
--- src/com/org/service/Codecoverage.groovy
+++ src/com/org/service/Codecoverage.groovy
@@ -4,18 +4,22 @@ package com.org.service
 //import com.org.exception.*
 import com.org.log.Logger
 import com.org.log.LogLevel
+import com.org.constant.GlobalVars
+import com.org.utils.PublishHtml
 
 class Codecoverage implements Serializable{
 Script mainScript
 Map specs
 Map config
 Logger logger
+PublishHtml publishHtml
 
   def Codecoverage(Script mainScript, Map specs, Map config){
   this.mainScript = mainScript
   this.specs = specs
   this.config = config
   this.logger = new Logger(mainScript,"Codecoverage")
+  this.publishHtml = new PublishHtml(GlobalVars.publishHtml_allowMissing, GlobalVars.publishHtml_alwaysLinkToLastBuild, GlobalVars.publishHtml_keepAll, GlobalVars.publishHtml_codeCoverage_reportDir, GlobalVars.publishHtml_codeCoverage_reportFiles, GlobalVars.publishHtml_codeCoverage_reportName, GlobalVars.publishHtml_reportTitles)
   }
   void codecoverageCheckFunc(Map specs, Map config){
     if (specs.codeCoverage.isCodecoverageRequired && specs.containsKey("codeCoverage")){  
@@ -28,7 +32,8 @@ Logger logger
           logger.info "executing codeCoverage command: " + config.java.codecoverage.jacoco.command 
           mainScript.sh config.java.codecoverage.jacoco.command 
         } 
-        mainScript.publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: './target/site/jacoco/', reportFiles: 'index.html', reportName: 'Code Coverage Report', reportTitles: ''])
+        publishHtml.publishHtmlFunc(GlobalVars.publishHtml_allowMissing, GlobalVars.publishHtml_alwaysLinkToLastBuild, GlobalVars.publishHtml_keepAll, GlobalVars.publishHtml_codeCoverage_reportDir, GlobalVars.publishHtml_codeCoverage_reportFiles, GlobalVars.publishHtml_codeCoverage_reportName, GlobalVars.publishHtml_reportTitles)
+        //mainScript.publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: './target/site/jacoco/', reportFiles: 'index.html', reportName: 'Code Coverage Report', reportTitles: ''])
         logger.info "codeCoverage successfully completed."
       } else {
           logger.warn "unsupported tool. Please use jacoco."
