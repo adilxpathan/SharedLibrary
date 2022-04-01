74592cdbdd6320a69c00eefe5fcc16909cb5c66e
Adil Pathan <Adil.Pathan@lntinfotech.com>
2022-04-01 14:05:24 +0530

    email changes

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -21,12 +21,21 @@ def emailPipelineStatus(){
         
         //html_body = sh(script: "cat ${config.props.mailbodyhtml}", returnStdout: true).trim()
         //html_subject = sh(script: "cat ${config.props.emailsubhtml}", returnStdout: true).trim()
-        emailext attachmentsPattern: 'letsSolve*.png',
-        mimeType: 'text/html',
-        body: '${FILE, path="/resources/com/org/service/email/index.html"}',
-        from: 'ltipoctest@gmail.com',
+        
+        
+        //emailext attachmentsPattern: 'letsSolve*.png',
+        //mimeType: 'text/html',
+        //body: '${FILE, path="/resources/com/org/service/email/index.html"}',
+        //from: "ltipoctest@gmail.com",
+        //subject: 'TEST',
+        //to: "ltipoctest@gmail.com"
+
+        emailext 
+        body: 'TEST',
+        from: "ltipoctest@gmail.com",
         subject: 'TEST',
-        to: 'ltipoctest@gmail.com'
+        to: "ltipoctest@gmail.com"
+
 
     }catch(emailEx){
         logger.exception(emailEx, "failed to send email")
