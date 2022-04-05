c8a8c33e52c1dfa8f4a20049d6048e57baeae36e
Adil Pathan <Adil.Pathan@lntinfotech.com>
2022-04-05 18:11:29 +0530

    updated changes

---
+++ test/BuildJavascriptAppSpec.groovy
@@ -0,0 +1,22 @@
+import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
+import com.org.service.Build
+
+
+class BuildJavascriptAppSpec extends JenkinsPipelineSpecification {
+    def buildJavascriptApp = null
+    specs = [build.type: "java", build.tool: "maven", build.command: "mvn -version"]
+    config = [:]
+    def setup() {
+
+        BuildJavascriptAppSpec build = new new com.org.service.Build(this, specs, config)
+        //buildJavascriptApp = loadPipelineScriptForTest("vars/jenkinsfile.groovy")
+    }
+
+    def "[build] will run unit test if build.type is java"() {
+        when:
+            build build.type = 'java'
+        then:
+            1 * getPipelineMock("build.buildFunc.call")(specs, config)
+    }
+
+}
